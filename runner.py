1815590757fc4202ec8c8885b7de005c39cc729b
Kusal De Alwis <thisiskusal@gmail.com>
2020-10-25 12:45:00 -0400

    Base training code. Runs LeNet on MNIST

--- runner.py
+++ runner.py
@@ -8,6 +8,7 @@ import torchvision.transforms as transforms
 import torchvision.datasets as datasets
 import matplotlib.pyplot as plt
 
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 from archs.mnist import AlexNet, LeNet5, fc1, vgg, resnet
 
@@ -15,3 +16,47 @@ transform=transforms.Compose([transforms.ToTensor(),transforms.Normalize((0.1307
 train_dataset = datasets.MNIST('../data', train=True, download=True,transform=transform)
 test_dataset = datasets.MNIST('../data', train=False, transform=transform)
 
+batch_size = 64
+
+train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=0,drop_last=False)
+test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=batch_size, shuffle=False, num_workers=0,drop_last=True)
+
+model = LeNet5.LeNet5().to(device)
+
+optimizer = torch.optim.Adam(model.parameters(), weight_decay=1e-4)
+criterion = nn.CrossEntropyLoss()
+
+for name, param in model.named_parameters():
+    print(name, param.size())
+
+epochs = 300
+
+for epoch in range(1, epochs + 1):
+    print("Starting epoch: {}".format(epoch))
+    for batch_idx, (data, target) in enumerate(train_loader):
+        data, target = data.to(device), target.to(device)
+        optimizer.zero_grad()
+        output = model(data)
+        loss = criterion(output, target)
+        loss.backward()
+        optimizer.step()
+        if batch_idx % 20 == 0:
+            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                epoch, batch_idx * len(data), len(train_loader.dataset),
+                100. * batch_idx / len(train_loader), loss.item()))
+        if batch_idx == 100:
+            break
+    test_loss = 0
+    correct = 0
+    with torch.no_grad():
+        for data, target in test_loader:
+            data, target = data.to(device), target.to(device)
+            output = model(data)
+            test_loss += criterion(output, target, reduction='sum').item()  # sum up batch loss
+            pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
+            correct += pred.eq(target.view_as(pred)).sum().item()
+
+    test_loss /= len(test_loader.dataset)
+    print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
+        test_loss, correct, len(test_loader.dataset),
+        100. * correct / len(test_loader.dataset)))
